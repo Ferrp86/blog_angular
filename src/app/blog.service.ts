267c5a1ed7589b9432ffd3c02c18f3f8a0f6dcbc
Ferrp86 <freviriegop86@gmail.com>
2021-11-18 23:42:37 +0100

    Blog en angular terminado

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -10,13 +10,18 @@ export class BlogService {
   arrPublicacion: Post[];
 
   constructor() {
-    this.arrPublicacion = [
-      { titulo: 'Viaje a Londres', texto: 'Un viaje muy bonito a Londres. Foto Catedral de St. Paul', autor: 'Fernando', imagen: 'https://cdn.pixabay.com/photo/2021/08/12/05/19/cathedral-6539937_960_720.jpg', fecha: '18/11/2021', categoria: 'Viajes' }
-    ];
+    if (localStorage.getItem('arrPublicacion')) {
+      this.arrPublicacion = JSON.parse(localStorage.getItem('arrPublicacion')!);
+    } else {
+      this.arrPublicacion = [
+        { titulo: 'Viaje a Londres', texto: 'Un viaje muy bonito a Londres. Foto Catedral de St. Paul', autor: 'Fernando', imagen: 'https://cdn.pixabay.com/photo/2021/08/12/05/19/cathedral-6539937_960_720.jpg', fecha: '18/11/2021', categoria: 'Viajes' }
+      ];
+    }
   }
 
   agregarPost(nuevaPublicacion: Post) {
     this.arrPublicacion.push(nuevaPublicacion);
+    localStorage.setItem('arrPublicacion', JSON.stringify(this.arrPublicacion));
   }
 
   getAllPost(): Post[] {
